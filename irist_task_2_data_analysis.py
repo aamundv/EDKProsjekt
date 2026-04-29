516dadb0ef48a7c03d265e5bd338a73706af4567
Kirlu3 <ulrikgarmarkanker@gmail.com>
2026-04-29 14:38:42 +0200

    changed from training to test accuracy in the task 2 analysis

--- irist_task_2_data_analysis.py
+++ irist_task_2_data_analysis.py
@@ -7,7 +7,7 @@ results = pd.read_pickle("iris_2_results_df.pkl")
 for index, row in results.iterrows():
     print(f"Features: {row['Features']}")
     print(f"Training Set: {row['Training Set']}")
-    print(f"Training Accuracy: {row['Training Accuracy']:.4f}")
+    print(f"Test Accuracy: {row['Test Accuracy']:.4f}")
     print("Confusion Matrix:")
     print(row["Confusion Matrix"])
     print("-" * 40)
